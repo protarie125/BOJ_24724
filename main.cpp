b204f4365642244709176a8471aa47edbee69038
yasur <yasur@LAPTOP-GK9UFHLD>
2022-11-15 09:38:30 +0900

    초기 커밋.

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	int t;
+	cin >> t;
+	for (auto i = 1; i <= t; ++i) {
+		cout << "Material Management " << i << '\n';
+		cout << "Classification ---- End!\n";
+
+		int n;
+		cin >> n;
+
+		int a, b;
+		cin >> a >> b;
+		while (0 < (n--)) {
+			int u, v;
+			cin >> u >> v;
+		}
+	}
+
+	return 0;
+}
